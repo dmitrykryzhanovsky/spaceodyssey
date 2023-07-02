543b26e8c68c0dc5e409b2046947b48a35923bda
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-07-02 21:13:33 +0300

    Удалил неудачные файлы

--- SpaceOdyssey.Cosmodynamics.NFX/MathConst.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Archimedes
-{
-    public static class MathConst
-    {
-        public static readonly double M_SQRT_2 = Math.Sqrt (2.0);
-    }
-}
--- SpaceOdyssey.Cosmodynamics.NFX/PhysConst.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Archimedes
-{
-    public static class PhysConst
-    {
-        public const double G = 6.67430e-11;
-    }
-}

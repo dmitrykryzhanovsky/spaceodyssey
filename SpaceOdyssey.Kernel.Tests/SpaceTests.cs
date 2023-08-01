b43dd495a94f696df53d746c236667ef2122b0fe
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-08-01 22:46:13 +0300

    Пытался прогнать тесты

---
+++ SpaceOdyssey.Kernel.Tests/SpaceTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SpaceOdyssey;
+
+using Archimedes;
+
+namespace SpaceOdyssey.Tests
+{
+    [TestClass ()]
+    public class SpaceTests
+    {
+        [TestMethod ()]
+        public void EqToEcTransformTest_Sirius ()
+        {
+            double ascension   = Trigonometry.CompoundHourHMinHSec (6, 45, 8.92);
+            double declination = Trigonometry.CompoundDegMinSec (-16, 42, 58.02);
+
+            Polar3 eq = Polar3.CreateFineUnit (Trigonometry.DegToRad (declination), Trigonometry.HourToRad (ascension));
+
+            Assert.Fail ();
+        }
+    }
+}

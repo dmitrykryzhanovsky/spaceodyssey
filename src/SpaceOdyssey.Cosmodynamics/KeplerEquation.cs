4ed8fb0402776de0afe17a23c2ac5a18e46682d3
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2023-12-31 18:31:35 +0300

    Реализовал эллиптическое уравнение Кеплера

---
+++ src/SpaceOdyssey.Cosmodynamics/KeplerEquation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Archimedes.Numerical;
+
+namespace SpaceOdyssey.Cosmodynamics
+{
+    public static class KeplerEquation
+    {
+        private static double EllipticEquation (double E, params double [] param)
+        {
+            return E - param [0] * Math.Sin (E) - param [1];
+        }
+
+        private static double EllipticDerivative (double E, params double [] param)
+        {
+            return 1.0 - param [0] * Math.Cos (E);
+        }
+
+        public static double Elliptic (double M, double eccentricity, double epsilon)
+        {
+            return NonLinearEquation.Newton (EllipticEquation, EllipticDerivative, epsilon, M, eccentricity, M);
+        }
+    }
+}

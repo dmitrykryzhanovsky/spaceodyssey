9c5da6362359b881576f6320ed8c6bae56c6b713
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-01-06 09:09:46 +0300

    Протестировал HyperbolicOrbit

--- src/SpaceOdyssey.Cosmodynamics/HyperbolicOrbit.cs
+++ src/SpaceOdyssey.Cosmodynamics/HyperbolicOrbit.cs
@@ -18,10 +18,21 @@
             get => _asymptote;
         }
 
+        #region Constructors
+
         public HyperbolicOrbit (IGravityMass orbitalCenter) : base (orbitalCenter)
         {
         }
 
+        #endregion
+
+        /// <summary>
+        /// Инициализация элементов орбиты через эксцентриситет eccentricity и среднее движение meanMotion.
+        /// </summary>
+        /// <param name="eccentricity">Должен быть больше 1.</param>
+        /// <param name="meanMotion">Должно быть больше 0.</param>
+        /// <exception cref="EllipseEccentricityOutOfRangeException">Если eccentricity не соответствует гиперболе (меньше либо равен 1).</exception>
+        /// <exception cref="TemporalElementNegativeException">Если meanMotion <= 0.</exception>
         public override void SetOrbitalElementsByMeanMotion (double eccentricity, double meanMotion)
         {
             CheckEccentricity (eccentricity);
@@ -38,6 +49,13 @@
             _amin = _a * (1.0 - _e);
         }
 
+        /// <summary>
+        /// Инициализация элементов орбиты через эксцентриситет eccentricity и расстояние в перицентре periapsis.
+        /// </summary>
+        /// <param name="eccentricity">Должен быть больше 1.</param>
+        /// <param name="periapsis">Должно быть больше 0.</param>
+        /// <exception cref="EllipseEccentricityOutOfRangeException">Если eccentricity не соответствует гиперболе (меньше либо равен 1).</exception>
+        /// <exception cref="DimensionalElementNegativeException">Если periapsis <= 0.</exception>
         public override void SetOrbitalElementsByPeriapsis (double eccentricity, double periapsis)
         {
             CheckEccentricity (eccentricity);
@@ -55,6 +73,13 @@
             _n = CosmodynamicsFormulae.MeanMotionBySemiMajorAxisForHyperbola (K, _a);
         }
 
+        /// <summary>
+        /// Инициализация элементов орбиты через расстояние в перицентре periapsis и среднее движение meanMotion.
+        /// </summary>
+        /// <param name="periapsis">Должно быть больше 0.</param>
+        /// <param name="meanMotion">Должно быть больше 0.</param>
+        /// <exception cref="DimensionalElementNegativeException">Если periapsis <= 0.</exception>
+        /// <exception cref="TemporalElementNegativeException">Если meanMotion <= 0.</exception>
         public override void SetOrbitalElementsByPeriapsisAndMeanMotion (double periapsis, double meanMotion)
         {
             CheckPeriapsis (periapsis);
@@ -67,7 +92,7 @@
             _p = _amin * (2.0 - _amin / _a);
 
             _e         = (_a - _amin) / _a;
-            _e2factor  = double.Sqrt (_amin * (_amin - 2.0 * _a)) / _a;
+            _e2factor  = double.Sqrt (_amin * (_amin - 2.0 * _a)) / -_a;
             _asymptote = double.Acos (_a / (_amin - _a));            
         }
 

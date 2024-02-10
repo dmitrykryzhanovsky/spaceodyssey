178be416ffd7b44980d8838ad24b147d52c7a903
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2024-02-10 10:12:00 +0300

    Написал вычисление наклона земной оси

---
+++ src/SpaceOdyssey.Kernel.Tests/AxialTiltTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpaceOdyssey.Tests
+{
+    [TestClass ()]
+    public class AxialTiltTests
+    {
+        [TestMethod ()]
+        public void GetTiltTest_JD2000 ()
+        {
+            double T = 0.0;
+
+            double expected = 0.40909280420293639;
+
+            double actual = AxialTilt.GetTilt (T);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetTiltTest_T05 ()
+        {
+            double T = 0.5;
+
+            double expected = 0.40897932182413950;
+
+            double actual = AxialTilt.GetTilt (T);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetTiltTest_Tminus05 ()
+        {
+            double T = -0.5;
+
+            double expected = 0.40920628515153292;
+
+            double actual = AxialTilt.GetTilt (T);
+
+            Assert.AreEqual (expected, actual);
+        }
+    }
+}

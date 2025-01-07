baaef91519d8c47534e7109f50f4b985fdc44519
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-01-07 16:09:09 +0300

    Расчёт прецессии по долготе

---
+++ src/SpaceOdyssey.Kernel.Tests/PrecessionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpaceOdyssey.Tests
+{
+    [TestClass ()]
+    public class PrecessionTests
+    {
+        [TestMethod ()]
+        public void GetLongitudePrecessionTest ()
+        {
+            double T0 = 0.02;
+            double dT = 0.23;
+
+            double expected = 0.0056081366477320803;
+
+            double actual = Precession.GetLongitudePrecession (T0, dT);
+
+            Assert.AreEqual (expected, actual);
+        }
+        //0,058778732564
+        //0,000000073002 
+        //0,058778659562
+        //-0,0000000168
+        //1156,761219051698
+        //0,00560813664773208026155446014945
+    }
+}
---
+++ src/SpaceOdyssey.Kernel/Precession.cs
@@ -0,0 +1,24 @@
+﻿using Archimedes;
+
+namespace SpaceOdyssey
+{
+    public static class Precession
+    {
+        private static readonly double [] LongitudePrecession_Series = new double []
+        {
+            Trigonometry.SecToRad (5029.0966),
+            Trigonometry.SecToRad (   2.22226),
+            Trigonometry.SecToRad (  -0.000042),
+            Trigonometry.SecToRad (   1.11113),
+            Trigonometry.SecToRad (  -0.000042),
+            Trigonometry.SecToRad (  -0.000006)
+        };
+
+        public static double GetLongitudePrecession (double T0, double dT)
+        {
+            return dT * (dT * (dT * LongitudePrecession_Series [5] + 
+                              (T0 * LongitudePrecession_Series [4] + LongitudePrecession_Series [3])) + 
+                        (T0 * (T0 * LongitudePrecession_Series [2] + LongitudePrecession_Series [1]) + LongitudePrecession_Series [0]));
+        }
+    }
+}

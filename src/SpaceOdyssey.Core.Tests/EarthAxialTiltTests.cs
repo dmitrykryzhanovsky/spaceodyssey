88fc5499285320820df2ba9ad7257fb5fcf68b00
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-01-18 20:20:34 +0300

    Сделал вычисление наклона земной оси

---
+++ src/SpaceOdyssey.Core.Tests/EarthAxialTiltTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpaceOdyssey.Tests
+{
+    [TestClass ()]
+    public class EarthAxialTiltTests
+    {
+        [TestMethod ()]
+        public void ComputeDE200Test ()
+        {
+            double T = 5;
+
+            double expected = 84147.584875;
+
+            double actual = EarthAxialTilt.ComputeDE200 (T);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void ComputeP03Test ()
+        {
+            double T = 5;
+
+            double expected = 84147.467506875;
+
+            double actual = EarthAxialTilt.ComputeP03 (T);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void ComputeLaskarTest ()
+        {
+            double T = 5;
+
+            double expected = 84147.6471314996315;
+
+            double actual = EarthAxialTilt.ComputeLaskar (T);
+
+            Assert.AreEqual (expected, actual);
+        }
+    }
+}
---
+++ src/SpaceOdyssey.Core/EarthAxialTilt.cs
@@ -0,0 +1,48 @@
+﻿using Archimedes;
+
+namespace SpaceOdyssey
+{
+    public static class EarthAxialTilt
+    {
+        private const double ApproximationArcmin = 84360;
+
+        private static readonly double [] ArcsecondSeries_DE200  = new double [] {   21.448, 
+                                                                                    -46.8150, 
+                                                                                     -0.00059, 
+                                                                                      0.001813 };
+
+        private static readonly double [] ArcsecondSeries_P03    = new double [] {   21.406, 
+                                                                                    -46.836769, 
+                                                                                     -0.0001831, 
+                                                                                      0.00200340, 
+                                                                                     -0.000000576, 
+                                                                                     -0.0000000434 };
+
+        private static readonly double [] ArcsecondSeries_Laskar = new double [] {   21.448, 
+                                                                                  -4680.93, 
+                                                                                     -1.55, 
+                                                                                   1999.25, 
+                                                                                    -51.38, 
+                                                                                   -249.67, 
+                                                                                    -39.05, 
+                                                                                      7.12, 
+                                                                                     27.87, 
+                                                                                      5.79, 
+                                                                                      2.45 };
+
+        public static double ComputeDE200 (double T)
+        {
+            return ApproximationArcmin + PolynomialAlgorithm.ComputeCube (T, ArcsecondSeries_DE200);
+        }
+
+        public static double ComputeP03 (double T)
+        {
+            return ApproximationArcmin + PolynomialAlgorithm.Compute (T, ArcsecondSeries_P03);
+        }
+
+        public static double ComputeLaskar (double T)
+        {
+            return ApproximationArcmin + PolynomialAlgorithm.Compute (T / 100.0, ArcsecondSeries_Laskar);
+        }
+    }
+}

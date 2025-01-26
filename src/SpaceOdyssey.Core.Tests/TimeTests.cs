4ec6c2a98411c3c0cd7122a1107031fe99a485b2
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-01-26 20:26:06 +0300

    Сделал и протестировал вычисление JDN для григорианского календаря

---
+++ src/SpaceOdyssey.Core.Tests/TimeTests.cs
@@ -0,0 +1,125 @@
+﻿using SpaceOdyssey;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpaceOdyssey.Tests
+{
+    [TestClass ()]
+    public class TimeTests
+    {
+        [TestMethod ()]
+        public void GetCalendarStyleTest_BeforeFirstGregorianYear ()
+        {
+            ECalendarStyle expected = ECalendarStyle.Julian;
+
+            ECalendarStyle actual = Time.GetCalendarStyle (year: 1581, month: 12, day: 31);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetCalendarStyleTest_FirstGregorianYear_BeforeFirstGregorianMonth ()
+        {
+            ECalendarStyle expected = ECalendarStyle.Julian;
+
+            ECalendarStyle actual = Time.GetCalendarStyle (year: 1582, month: 9, day: 30);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetCalendarStyleTest_FirstGregorianYear_FirstGregorianMonth_BeforeFirstGregorianDay ()
+        {
+            ECalendarStyle expected = ECalendarStyle.Julian;
+
+            ECalendarStyle actual = Time.GetCalendarStyle (year: 1582, month: 10, day: 4);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetCalendarStyleTest_FirstGregorianYear_FirstGregorianMonth_FirstGregorianDay ()
+        {
+            ECalendarStyle expected = ECalendarStyle.Gregorian;
+
+            ECalendarStyle actual = Time.GetCalendarStyle (year: 1582, month: 10, day: 15);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetCalendarStyleTest_FirstGregorianYear_FirstGregorianMonth_AfterFirstGregorianDay ()
+        {
+            ECalendarStyle expected = ECalendarStyle.Gregorian;
+
+            ECalendarStyle actual = Time.GetCalendarStyle (year: 1582, month: 10, day: 16);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetCalendarStyleTest_FirstGregorianYear_AfterFirstGregorianMonth ()
+        {
+            ECalendarStyle expected = ECalendarStyle.Gregorian;
+
+            ECalendarStyle actual = Time.GetCalendarStyle (year: 1582, month: 11, day: 1);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetCalendarStyleTest_AfterFirstGregorianYear ()
+        {
+            ECalendarStyle expected = ECalendarStyle.Gregorian;
+
+            ECalendarStyle actual = Time.GetCalendarStyle (year: 1583, month: 1, day: 1);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetJDNTest ()
+        {
+            Assert.Fail ();
+        }
+
+        [TestMethod ()]
+        public void GetJDNTest_Gregorian_JulianPeriodBegin ()
+        {
+            int expected = 0;
+
+            int actual = Time.GetJDN (year: -4713, month: 11, day: 24);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetJDNTest_Gregorian_FirstGregorianDay ()
+        {
+            int expected = 2299161;
+
+            int actual = Time.GetJDN (year: 1582, month: 10, day: 15);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetJDNTest_Gregorian_J2000 ()
+        {
+            int expected = 2451545;
+
+            int actual = Time.GetJDN (year: 2000, month: 1, day: 1);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void GetJDNTest_Gregorian_T60 ()
+        {
+            int expected = 2460671;
+
+            int actual = Time.GetJDN (year: 2024, month: 12, day: 26);
+
+            Assert.AreEqual (expected, actual);
+        }
+    }
+}
--- src/SpaceOdyssey.Core/Time.cs
+++ src/SpaceOdyssey.Core/Time.cs
@@ -2,6 +2,11 @@
 {
     public static class Time
     {
+        // Первая дата григорианского календа рая (нового стиля) – 15 октября 1582 г.
+        private const int FirstGregorianYear  = 1582;
+        private const int FirstGregorianMonth = 10;
+        private const int FirstGregorianDay   = 15;
+
         /// <summary>
         /// Эпоха J2000 – полдень 1 января 2000 г. по земному времени (1.5 января 2000 г.)
         /// </summary>
@@ -12,6 +17,41 @@
         /// </summary>
         public const double B1950 = 2433282.4235;
 
+        /// <summary>
+        /// Определить стиль календаря (юлианский или грегорианский) для заданной даты (year, month, day).
+        /// </summary>
+        public static ECalendarStyle GetCalendarStyle (int year, int month, int day)
+        {
+            if ((year > FirstGregorianYear) ||
+                ((year == FirstGregorianYear) && ((month > FirstGregorianMonth) ||
+                                                  ((month == FirstGregorianMonth) && (day >= FirstGregorianDay)))))
+            {
+                return ECalendarStyle.Gregorian;
+            }
 
+            else return ECalendarStyle.Julian;
+        }
+
+        public static int GetJDN (int year, int month, int day, ECalendarStyle calendarStyle)
+        {
+            int a = (14 - month) / 12;
+            int y = year + 4800 - a;
+            int m = month + 12 * a - 3;
+
+            int commonTerm = day + ((153 * m + 2) / 5) + 365 * y + (y / 4);
+
+            if (calendarStyle == ECalendarStyle.Gregorian) return commonTerm - (y / 100) + (y / 400) - 32045;
+
+            else return commonTerm - 32083;
+        }
+
+        public static int GetJDN (int year, int month, int day)
+        {
+            int a = (14 - month) / 12;
+            int y = year + 4800 - a;
+            int m = month + 12 * a - 3;
+
+            return day + ((153 * m + 2) / 5) + 365 * y + (y / 4) - (y / 100) + (y / 400) - 32045;
+        }
     }
 }

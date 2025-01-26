1b2733a2fd95fe29ab85df4e92f39832ed21887c
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-01-26 19:40:32 +0300

    Добавил эпохи j2000 и b1950

---
+++ src/SpaceOdyssey.Core/Time.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceOdyssey
+{
+    public static class Time
+    {
+        /// <summary>
+        /// Эпоха J2000 – полдень 1 января 2000 г. по земному времени (1.5 января 2000 г.)
+        /// </summary>
+        public const double J2000 = 2451545.0;
+
+        /// <summary>
+        /// Эпоха B1950 – начало 1950 бесселева года (0.923 января 1950 г.)
+        /// </summary>
+        public const double B1950 = 2433282.4235;
+
+
+    }
+}

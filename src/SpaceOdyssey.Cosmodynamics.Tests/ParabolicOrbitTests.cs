fdadc77aae8f53ed2055f12f0c1c0c387cd485fa
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-03-08 20:40:22 +0300

    Протестировал ParabolicOrbit

---
+++ src/SpaceOdyssey.Cosmodynamics.Tests/ParabolicOrbitTests.cs
@@ -0,0 +1,209 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpaceOdyssey.Cosmodynamics.Tests
+{
+    [TestClass ()]
+    public class ParabolicOrbitTests
+    {
+        [TestMethod ()]
+        public void CreateTest_Common ()
+        {
+            double amin = 2.0;
+
+            ParabolicOrbit target = ParabolicOrbit.Create (amin);
+
+            Assert.AreEqual (4.0, target.P);
+            Assert.AreEqual (1.0, target.E);
+            Assert.AreEqual (2.0, target.Amin);
+            Assert.AreEqual (double.Pi, target.Asymptote);
+        }
+
+        [TestMethod ()]
+        public void CreateTest_AminZero ()
+        {
+            double amin = 0.0;
+
+            bool wasException = false;
+
+            try
+            {
+                ParabolicOrbit target = ParabolicOrbit.Create (amin);
+            }
+
+            catch (ArgumentOutOfRangeException)
+            {
+                wasException = true;
+            }
+
+            Assert.AreEqual (true, wasException);
+        }
+
+        [TestMethod ()]
+        public void CreateTest_AminNegative ()
+        {
+            double amin = -2.0;
+
+            bool wasException = false;
+
+            try
+            {
+                ParabolicOrbit target = ParabolicOrbit.Create (amin);
+            }
+
+            catch (ArgumentOutOfRangeException)
+            {
+                wasException = true;
+            }
+
+            Assert.AreEqual (true, wasException);
+        }
+
+        [TestMethod ()]
+        public void RadiusTest_Anomaly0 ()
+        {
+            double amin        = 2.0;
+            double trueAnomaly = 0.0;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double expected = 2.0;
+
+            double actual = orbit.Radius (trueAnomaly);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void RadiusTest_AnomalyPI_3 ()
+        {
+            double amin        = 2.0;
+            double trueAnomaly = double.Pi / 3.0;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double expected = 8.0 / 3.0;
+
+            double actual = orbit.Radius (trueAnomaly);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void RadiusTest_AnomalyPI_2 ()
+        {
+            double amin        = 2.0;
+            double trueAnomaly = double.Pi / 2.0;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double expected = 4.0;
+
+            double actual = orbit.Radius (trueAnomaly);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void RadiusTest_Anomaly2PI_3 ()
+        {
+            double amin        = 2.0;
+            double trueAnomaly = 2.0 * double.Pi / 3.0;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double expected = 8.0;
+
+            double actual = orbit.Radius (trueAnomaly);
+
+            Assert.AreEqual (expected, actual, 1.0e-14);
+        }
+
+        [TestMethod ()]
+        public void RadiusTest_AnomalyPI ()
+        {
+            double amin        = 2.0;
+            double trueAnomaly = double.Pi;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double actual = orbit.Radius (trueAnomaly);
+
+            Assert.AreEqual (true, double.IsPositiveInfinity (actual));
+        }
+
+        [TestMethod ()]
+        public void TrueAnomalyTest_Anomaly0 ()
+        {
+            double amin = 2.0;
+            double r    = 2.0;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double expected = 0.0;
+
+            double actual = orbit.TrueAnomaly (r);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void TrueAnomalyTest_AnomalyPI_3 ()
+        {
+            double amin = 2.0;
+            double r    = 8.0 / 3.0;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double expected = double.Pi / 3.0;
+
+            double actual = orbit.TrueAnomaly (r);
+
+            Assert.AreEqual (expected, actual, 1.0e-15);
+        }
+
+        [TestMethod ()]
+        public void TrueAnomalyTest_AnomalyPI_2 ()
+        {
+            double amin = 2.0;
+            double r    = 4.0;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double expected = double.Pi / 2.0;
+
+            double actual = orbit.TrueAnomaly (r);
+
+            Assert.AreEqual (expected, actual);
+        }
+
+        [TestMethod ()]
+        public void TrueAnomalyTest_Anomaly2PI_3 ()
+        {
+            double amin = 2.0;
+            double r    = 8.0;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double expected = 2.0 * double.Pi / 3.0;
+
+            double actual = orbit.TrueAnomaly (r);
+
+            Assert.AreEqual (expected, actual, 1.0e-15);
+        }
+
+        [TestMethod ()]
+        public void TrueAnomalyTest_AnomalyPI ()
+        {
+            double amin = 2.0;
+            double r    = double.PositiveInfinity;
+
+            ParabolicOrbit orbit = ParabolicOrbit.Create (amin);
+
+            double expected = double.Pi;
+
+            double actual = orbit.TrueAnomaly (r);
+
+            Assert.AreEqual (expected, actual);
+        }
+    }
+}
--- src/SpaceOdyssey.Cosmodynamics/ParabolicOrbit.cs
+++ src/SpaceOdyssey.Cosmodynamics/ParabolicOrbit.cs
@@ -1,11 +1,18 @@
 ﻿namespace SpaceOdyssey.Cosmodynamics
 {
+    /// <summary>
+    /// Параболическая орбита.
+    /// </summary>
     public class ParabolicOrbit : KeplerOrbit
     {
         private const double ParabolicEccentricity = 1.0;
 
         private const double ParabolicAsymptote = double.Pi;
 
+        /// <summary>
+        /// Истинная аномалия, при которой параболическая орбита уходит на бесконечность.
+        /// </summary>
+        /// <remarks>У параболической орбиты всегда равна π радиан.</remarks>
         public double Asymptote
         {
             get => ParabolicAsymptote;
@@ -15,6 +22,11 @@
         {
         }
 
+        /// <summary>
+        /// Создаёт параболическую орбиту по расстоянию в перицентре.
+        /// </summary>
+        /// <param name="amin">Расстояние в перицентре – должно быть строго положительным.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Генерируется, если <paramref name="amin"/> меньше или равно 0.</exception>
         public static ParabolicOrbit Create (double amin)
         {
             if (amin <= 0.0) throw new ArgumentOutOfRangeException ();
@@ -27,11 +39,20 @@
             return 2.0 * amin;
         }
 
+        /// <summary>
+        /// Расстояние до центра тяготения при истинной аномалии trueAnomaly.
+        /// </summary>
+        /// <param name="trueAnomaly">Задаётся в радианах.</param>
         public override double Radius (double trueAnomaly)
         {
             return _p / (1.0 + double.Cos (trueAnomaly));
         }
 
+        /// <summary>
+        /// Истинная аномалия при расстоянии до центра тяготения r.
+        /// </summary>
+        /// <returns>Одному и тому же значению r соответствуют два значения истинной аномалии: x и -x. Данный метод возвращает 
+        /// неотрицательное значение из двух корректных.</returns>
         public override double TrueAnomaly (double r)
         {
             return double.Acos (_p / r - 1.0);

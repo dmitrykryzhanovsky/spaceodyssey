0948368e98b7db85f6a4d52a34edfb086d3d1958
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-03-09 19:13:44 +0300

    Протестировал HyperbolicOrbit

--- src/SpaceOdyssey.Cosmodynamics/HyperbolicOrbit.cs
+++ src/SpaceOdyssey.Cosmodynamics/HyperbolicOrbit.cs
@@ -1,16 +1,26 @@
 ﻿namespace SpaceOdyssey.Cosmodynamics
 {
+    /// <summary>
+    /// Гиперболическая орбита.
+    /// </summary>
     public class HyperbolicOrbit : KeplerOrbit
     {
         private double _a;
 
         private double _asymptote;
 
+        /// <summary>
+        /// Большая полуось.
+        /// </summary>
+        /// <remarks>Для гиперболической орбиты большая полуось отрицательна.</remarks>
         public double A
         {
             get => _a;
         }
 
+        /// <summary>
+        /// Истинная аномалия, при которой гиперболическая орбита уходит на бесконечность.
+        /// </summary>
         public double Asymptote
         {
             get => _asymptote;
@@ -22,6 +32,15 @@
             _asymptote = double.Acos (-1.0 / e);
         }
 
+        /// <summary>
+        /// Создаёт гиперболическую орбиту по расстоянию в перицентре и фокальному параметру.
+        /// </summary>
+        /// <param name="amin">Расстояние в перицентре – должно быть строго положительным.</param>
+        /// <param name="p">Фокальный параметр – должен быть строго больше расстояния в перицентре.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Генерируется, если имеет место хотя бы одно из условий:
+        /// – расстояние в перицентре <paramref name="amin"/> меньше либо равно 0
+        /// – фокальный параметр <paramref name="p"/> меньше либо равен расстоянию в перицентре <paramref name="amin"/>
+        /// </exception>
         public static HyperbolicOrbit CreateAminP (double amin, double p)
         {
             if (amin <= 0.0) throw new ArgumentOutOfRangeException ();
@@ -34,6 +53,15 @@
             return new HyperbolicOrbit (p, e, a, amin);
         }
 
+        /// <summary>
+        /// Создаёт гиперболическую орбиту по её расстоянию в перицентре и эксцентриситету.
+        /// </summary>
+        /// <param name="amin">Расстояние в перицентре – должно быть строго положительным.</param>
+        /// <param name="e">Эксцентриситет – должен быть строго больше 1.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Генерируется, если имеет место хотя бы одно из условий:
+        /// – расстояние в перицентре <paramref name="amin"/> меньше либо равно 0
+        /// – эксцентриситет <paramref name="e"/> меньше либо равен 1
+        /// </exception>
         public static HyperbolicOrbit CreateAminE (double amin, double e)
         {
             if (amin <= 0.0) throw new ArgumentOutOfRangeException ();

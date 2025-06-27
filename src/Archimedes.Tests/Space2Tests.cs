411692e6ffc68508d79d577eb843beefd9c85e85
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-06-27 22:04:48 +0300

    Протестировал Space2.ComputePolarComponents

---
+++ src/Archimedes.Tests/Space2Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Archimedes.Tests
+{
+    [TestClass ()]
+    public class Space2Tests
+    {
+        [TestMethod ()]
+        public void ComputePolarComponentsTest ()
+        {
+            double x = -3;
+            double y = -4;
+
+            double expectedR       =  5;
+            double expectedHeading = -2.2142974355881810;
+
+            (double r, double heading) actual = Space2.ComputePolarComponents (x, y);
+
+            Assert.AreEqual (expectedR, actual.r);
+            Assert.AreEqual (expectedHeading, actual.heading);
+        }
+    }
+}
--- src/Archimedes/Space2.cs
+++ src/Archimedes/Space2.cs
@@ -2,6 +2,9 @@
 {
     public static class Space2
     {
+        /// <summary>
+        /// Вычисляет полярные координаты для пары декартовых координат (x; y).
+        /// </summary>
         public static (double r, double heading) ComputePolarComponents (double x, double y)
         {
             double r = double.Hypot (x, y);

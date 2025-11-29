bbc30eac5a797515ece5ddbc584aeb0b527c178a
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2025-11-29 20:29:48 +0300

    Рефакторинг

---
+++ src/SpaceOdyssey.Cosmodynamics/Astrodata.cs
@@ -0,0 +1,27 @@
+﻿namespace SpaceOdyssey.Cosmodynamics
+{
+    public static class Astrodata
+    {
+        public static class Sun
+        {
+            /// <summary>
+            /// Локальная гравитационная постоянная, связанная с Солнцем, в единицах системы СИ.
+            /// </summary>
+            public const double GM_SI = 1.32712440018e+20;
+
+            /// <summary>
+            /// Гауссова гравитационная постоянная.
+            /// </summary>
+            /// <remarks>Квадратный корень локальной гравитационной постоянной, связанной с Солнцем, выраженный в единицах (а.е., сутки))</remarks>
+            public const double GaussianSqrtGM = 0.01720209895;
+        }
+
+        public static class Earth
+        {
+            /// <summary>
+            /// Локальная гравитационная постоянная, связанная с Землёй, в единицах системы СИ.
+            /// </summary>
+            public const double GM_SI = 3.986004418e+14;
+        }        
+    }
+}

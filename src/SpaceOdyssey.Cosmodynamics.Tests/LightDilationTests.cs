c523290e22115a143a12b154490beb97e9f229a1
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2026-01-05 21:04:21 +0300

    Дальше кодил

--- src/SpaceOdyssey.Cosmodynamics.Tests/LightDilationTests.cs
+++ src/SpaceOdyssey.Cosmodynamics.Tests/LightDilationTests.cs
@@ -21,5 +21,20 @@ namespace SpaceOdyssey.Cosmodynamics.Tests
 
             Assert.AreEqual (20.0316667, actual, 1.0e-4);
         }
+
+        [TestMethod ()]
+        public void ComputeDilatedPositionTest ()
+        {
+            Polar3 marsP = new Polar3 (r: 360322000000,
+                                         latitude: Trigonometry.DegToRad (-0.91033333333333333),
+                                         longitude: Trigonometry.DegToRad (285.04088888888888889));
+            Vector3 marsC = marsP.GetCartesian ();
+            Vector3 earthC = Vector3.Origin;
+
+            double actual = LightDilation.ComputeLightDilation (earthC, marsC, PhysConst.LightSpeed);
+            actual /= 60.0;
+
+            Assert.AreEqual (20.0316667, actual, 1.0e-4);
+        }
     }
 }
--- src/SpaceOdyssey.Cosmodynamics/LightDilation.cs
+++ src/SpaceOdyssey.Cosmodynamics/LightDilation.cs
@@ -16,5 +16,21 @@ namespace SpaceOdyssey.Cosmodynamics
         {
             return Space3.Distance (observerPosition, sattelitePosition) / lightSpeed;
         }
+
+        /// <summary>
+        /// Вычисляет положение спутника (наблюдаемого тела) в системе координат, связанной с наблюдателем, с поправкой на 
+        /// световую задержку.
+        /// </summary>
+        /// <param name="observerPosition">Положение наблюдателя в глобальной системе координат.</param>
+        /// <param name="sattelitePosition">Положение спутника (наблюдаемого теле) в глобальной системе координат.</param>
+        /// <param name="satteliteVelocity">Скорость спутника (наблюдаемого теле) в глобальной системе координат.</param>
+        /// <param name="dilation">Световая задержка спутника относительно наблюдателя.</param>
+        /// <returns>Вектор от наблюдателя к положению спутника, которое он занимал dilation время назад (то, которое наблюдатель 
+        /// видит вследствие световой задержки в заданный момент времени).</returns>
+        public static Vector3 ComputeDilatedPosition (Vector3 observerPosition, Vector3 sattelitePosition, 
+            Vector3 satteliteVelocity, double dilation)
+        {
+            return -observerPosition + sattelitePosition - satteliteVelocity * dilation;
+        }
     }
 }

f2c9b2e6798963d0726d8a3d8b68b9f334258bc9
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2026-01-05 08:10:25 +0300

    Тестирование

--- src/SpaceOdyssey.Cosmodynamics.Tests/LightDilationTests.cs
+++ src/SpaceOdyssey.Cosmodynamics.Tests/LightDilationTests.cs
@@ -1,6 +1,6 @@
-﻿using Archimedes;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Archimedes;
 
 namespace SpaceOdyssey.Cosmodynamics.Tests
 {
@@ -10,29 +10,16 @@ namespace SpaceOdyssey.Cosmodynamics.Tests
         [TestMethod ()]
         public void ComputeLightDilationTest ()
         {
-            Mass sun   = Mass.CreateByGM (Astrodata.ConvertSIToMetreDay (Astrodata.Sun.GM_SI));
-            Mass earth = Mass.CreateByGM (Astrodata.ConvertSIToMetreDay (Astrodata.Earth.GM_SI));
-            Mass mars  = Mass.CreateByGM (Astrodata.ConvertSIToMetreDay (Astrodata.Mars.GM_SI));
+            Polar3 marsP   = new Polar3 (r: 360322000000, 
+                                         latitude: Trigonometry.DegToRad (-0.91033333333333333), 
+                                         longitude: Trigonometry.DegToRad (285.04088888888888889));            
+            Vector3 marsC  = marsP.GetCartesian ();
+            Vector3 earthC = Vector3.Origin;
 
-            double t0 = Time.GetJD (DateTime.Now.ToUniversalTime ());
+            double actual = LightDilation.ComputeLightDilation (earthC, marsC, PhysConst.LightSpeed);
+            actual /= 60.0;
 
-            EllipticOrbit earthOrbit = EllipticOrbit.CreateBySemiMajorAxis (sun, earth, e: 0.01671022, a: 1.00000011 * AstroConst.AU);
-            earthOrbit.SetMeanAnomalyForJ2000 (Trigonometry.DegToRad (-2.48284));
-            earthOrbit.SetOrientation (inclination: Trigonometry.DegToRad (0.00005), 
-                                       ascendingNode: Trigonometry.DegToRad (-11.26064), 
-                                       periapsisArgument: Trigonometry.DegToRad (114.20783));
-
-            EllipticOrbit marsOrbit = EllipticOrbit.CreateBySemiMajorAxis (sun, earth, e: 0.09341233, a: 1.52366231 * AstroConst.AU);
-            marsOrbit.SetMeanAnomalyForJ2000 (Trigonometry.DegToRad (19.41248));
-            marsOrbit.SetOrientation (inclination: Trigonometry.DegToRad (1.85061),
-                                      ascendingNode: Trigonometry.DegToRad (49.57854),
-                                      periapsisArgument: Trigonometry.DegToRad (286.46230));
-
-            double halfEpsilon = 1.0 / (24.0 * 60.0 * 2.0);
-
-            double actual = LightDilation.ComputeLightDilation (t0, earthOrbit, marsOrbit, halfEpsilon);
-
-            int y = 4;
+            Assert.AreEqual (20.0316667, actual, 1.0e-4);
         }
     }
 }
--- src/SpaceOdyssey.Cosmodynamics/LightDilation.cs
+++ src/SpaceOdyssey.Cosmodynamics/LightDilation.cs
@@ -4,38 +4,17 @@ namespace SpaceOdyssey.Cosmodynamics
 {
     public static class LightDilation
     {
-        public static double ComputeLightDilation (double t0, KeplerOrbit observer, KeplerOrbit sattelite, double halpEpsilon)
+        /// <summary>
+        /// Возвращает световую задержку между двумя векторами (наблюдателем и спутником) с общим началом координат.
+        /// </summary>
+        /// <param name="observerPosition">Положение наблюдателя.</param>
+        /// <param name="sattelitePosition">Положение спутника (наблюдаемого объекта).</param>
+        /// <param name="lightSpeed">Скорость света в тех единицах расстояния, в которых выражены вектора observerPosition и 
+        /// sattelitePosition, и тех единицах времени, в которых мы хотим получить значение световой задержки.</param>
+        public static double ComputeLightDilation (Vector3 observerPosition, Vector3 sattelitePosition, 
+            double lightSpeed)
         {
-            OrbitalPosition oBgein = observer.ComputePosition (t0);
-            OrbitalPosition sBegin = sattelite.ComputePosition (t0);
-            
-            oBgein.ComputeSpatialPositionCartesian ();
-            sBegin.ComputeSpatialPositionCartesian ();
-
-            double dBegin = Space3.Distance (oBgein.SpatialPositionCartesian, sBegin.SpatialPositionCartesian);
-            double e      = 2.0 * dBegin / AstroConst.LightSpeed_MD;
-            double t1     = t0 - e;            
-
-            while (e > halpEpsilon)
-            {
-                double          t = (t0 + t1) / 2.0;
-                OrbitalPosition o = observer.ComputePosition (t);
-                OrbitalPosition s = sattelite.ComputePosition (t);
-
-                o.ComputeSpatialPositionCartesian ();
-                s.ComputeSpatialPositionCartesian ();
-
-                double d = Space3.Distance (o.SpatialPositionCartesian, s.SpatialPositionCartesian);
-                double f = AstroConst.LightSpeed_MD * (t0 - t) - d;
-
-                if (f > 0.0) t1 = t;
-                else if (f < 0.0) t0 = t;
-                else return t;
-
-                e = t0 - t1;
-            }
-
-            return (t0 + t1) / 2.0;
+            return Space3.Distance (observerPosition, sattelitePosition) / lightSpeed;
         }
     }
 }

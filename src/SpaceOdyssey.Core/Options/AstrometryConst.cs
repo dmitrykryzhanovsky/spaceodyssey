2b2d53c686f5b0e2ffbd926edea911a08fa6840a
Dmitry Kryzhanovskiy <dmitry.kryzhanovsky@gmail.com>
2026-05-31 15:20:11 +0300

    Перенёс код

---
+++ src/SpaceOdyssey.Core/Options/AstrometryConst.cs
@@ -0,0 +1,22 @@
+﻿namespace SpaceOdyssey
+{
+    public static class AstrometryConst
+    {
+        // Период вращения Земли вокруг своей оси = 23 ч 56 мин 4,091 с.
+        private const double EarthRotationPeriodComponentHour = 23.0;
+        private const double EarthRotationPeriodComponentMin  = 56.0;
+        private const double EarthRotationPeriodComponentSec  = 4.091;
+
+        /// <summary>
+        /// Количество секунд в 1 периоде вращения Земли вокруг своей оси = 23 ч 56 мин 4,091 с.
+        /// </summary>
+        public static readonly double SecInEarthRotation = EarthRotationPeriodComponentHour * TimeUnits.SecInHour +
+                                                           EarthRotationPeriodComponentMin  * TimeUnits.SecInMin +
+                                                           EarthRotationPeriodComponentSec;
+
+        /// <summary>
+        /// Количество оборотов Земли вокруг своей оси в 1 сутках.
+        /// </summary>
+        public static readonly double EarthRotationInDay = TimeUnits.SecInDay / SecInEarthRotation;
+    }
+}
---
+++ src/SpaceOdyssey.Core/Options/Epochs.cs
@@ -0,0 +1,18 @@
+﻿namespace SpaceOdyssey
+{
+    /// <summary>
+    /// Юлианские дате эпох, важных для астрономических расчётов.
+    /// </summary>
+    public static class Epochs
+    {
+        /// <summary>
+        /// Эпоха J2000 – полдень 1 января 2000 г. по земному времени (1.5 января 2000 г.)
+        /// </summary>
+        public const double J2000 = 2451545.0;
+
+        /// <summary>
+        /// Эпоха B1950 – начало 1950 бесселева года (0.923 января 1950 г.)
+        /// </summary>
+        public const double B1950 = 2433282.4235;
+    }
+}
